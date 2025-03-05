1e57b9378527533d8d08fe482a107893b5921816
Clifford Decker <40838191+cliffman123@users.noreply.github.com>
2025-03-04 19:40:08 -0500

    WS 0.44

--- my-react-app/src/config.js
+++ my-react-app/src/config.js
@@ -1,7 +1,5 @@
 const config = {
-  apiUrl: process.env.REACT_APP_API_URL || 'https://kupo-nuts-api.onrender.com'
+  API_URL: 'https://kupo-nuts.onrender.com'
 };
-
-console.log('Current API URL:', config.apiUrl);
 
 export default config;
